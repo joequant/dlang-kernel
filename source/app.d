f9ffec7d0afb54e3712009ed465056459c226ff6
Joseph C Wang <joequant@gmail.com>
2020-11-23 01:25:20 +0800

    redo module to use module constructor

--- source/app.d
+++ source/app.d
@@ -3,19 +3,17 @@ import jupyter.wire.message : CompleteResult;
 import drepl;
 
 mixin Main!DLangBackend;
+typeof(interpreter(dmdEngine())) inpr;
+static this() {
+  inpr = interpreter(dmdEngine());
+}
 
-typeof(interpreter(dmdEngine())) inpr_in;
 struct DLangBackend {
     enum languageInfo = LanguageInfo("D", "0.0.1", ".d");
     int value;
-    typeof(interpreter(dmdEngine())) *inpr = null;
     ExecutionResult execute(in string code, scope IoPubMessageSender sender) {
         import std.conv: text;
-	if (inpr is null) {
-	   inpr_in = interpreter(dmdEngine()); 
-	   inpr = &inpr_in;
-	}
-	auto res = (*inpr).interpret(code);
+	auto res = inpr.interpret(code);
 	final switch (res.state) with(InterpreterResult.State)
         {
         case incomplete:
